ac9a5ad551b0bcaa476176f0b267f232b45a3dd1
Jophkins <sergebrew@gmail.com>
2023-02-18 23:51:05 +0300

    request page inputs layout

--- src/components/UserGallery/index.jsx
+++ src/components/UserGallery/index.jsx
@@ -124,15 +124,12 @@ const UserGallery = ({ photos }) => {
           <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
         </svg>
       </div>
-      <div className="row">
+      <div className={styles.gallery}>
         {photos.map((item) => {
           return (
-            <div
-              className="col-md-2 col-6 my-2 d-flex justify-content-center"
-              key={item.resource_id}>
+            <div className={styles.pics} key={item.resource_id}>
               {isLoading && <ClipLoader color="#198754" />}
               <img
-                className={`${styles.imgPreview} img-fluid p-1`}
                 style={{ display: isLoading ? 'none' : 'block' }}
                 onLoad={() => setIsLoading(false)}
                 onClick={() => openModal(item)}
